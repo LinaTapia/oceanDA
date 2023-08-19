d6090aac5bb1cbb4610be63f64b3aa26cb390a06
CatalinaTapiaa <tapiaa.catalina@gmail.com>
2023-08-19 19:12:55 -0400

    Agregando texto adicional

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hola Coder!</Text>
+      <Text style={styles.importantText}>Hola Coder!</Text>
+      <Text>Bienvenido a Desarrollo de Aplicaciones con React Native</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +17,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  importantText: {
+    fontSize: 24,
+    fontWeight: 'bold',
   },
 });
